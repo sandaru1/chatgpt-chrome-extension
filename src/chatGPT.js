80f051da3c20aaa8b0ded002ba80996dc418a81f
Sandaruwan Gunathilake <sandaruwan@gunathilake.com>
2023-05-10 19:32:58 +0530

    Updated to use the openai-web package

--- src/chatGPT.js
+++ src/chatGPT.js
@@ -1,79 +1,70 @@
-import {getAPIKey} from "./common.js"
+import { getAPIKey } from "./common.js"
 
-const EDIT_API_ENDPOINT = "https://api.openai.com/v1/edits";
-const EDIT_MODEL = "text-davinci-edit-001";
+import { Configuration, OpenAIApi } from "../node_modules/openai-web/dist/openai.module.min.js"
+
 const EDIT_INSTRUCTION = "Rewrite following text";
 
-const CHAT_COMPLETION_EDNPOINT = "https://api.openai.com/v1/chat/completions";
 const CHAT_COMPLETION_MODEL = "gpt-3.5-turbo";
-
-const MODELS_LIST = "https://api.openai.com/v1/models";
 
 export const USAGE_URL = "https://platform.openai.com/account/usage";
 
 export async function checkAPIkey(apiKey) {
-    const res = await fetch(MODELS_LIST, {
-        method: "GET",
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + apiKey
-        }
-    });
-    const json = await res.json();
-    if (json.error) {
+    const configuration = new Configuration({
+        apiKey: apiKey,
+    });                
+    const openai = new OpenAIApi(configuration);
+
+    try {
+        const response = await openai.listEngines();
+        return true;
+    } catch (e) {
         return false;
     }
-    return true;
-} 
+}
 
-async function chatGPT(api,body) {
+async function chatCompletion(messages) {
     const apiKey = await getAPIKey();
     if (apiKey != null && apiKey != "") {
-        const res = await fetch(api, {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + apiKey
-            },
-            body: JSON.stringify(body)
-        }); 
-        return await res.json();
+        const configuration = new Configuration({
+            apiKey: apiKey,
+        });
+        const openai = new OpenAIApi(configuration);
+
+        const response = await openai.createChatCompletion({
+            model: CHAT_COMPLETION_MODEL,
+            messages: messages,
+        });
+
+        return response.data;
     } else {
         chrome.runtime.openOptionsPage();
     }
     return null;
 }
 
-export async function rewriteText(input) {
-    /*return await chatGPT(EDIT_API_ENDPOINT,{
-        "model":EDIT_MODEL,
-        "input":input,
-        "instruction":EDIT_INSTRUCTION
-    });*/
 
-    return await chatGPT(CHAT_COMPLETION_EDNPOINT,{
-        "model":CHAT_COMPLETION_MODEL,
-        "messages":[
+export async function rewriteText(input) {
+    return await chatCompletion(
+        [
             {
-                "role":"user",
-                "content":EDIT_INSTRUCTION
+                "role": "user",
+                "content": EDIT_INSTRUCTION
             },
             {
-                "role":"user",
-                "content":input
+                "role": "user",
+                "content": input
             }
         ]
-    });
+    );
 }
 
 export async function askChatGPT(input) {
-    return await chatGPT(CHAT_COMPLETION_EDNPOINT,{
-        "model":CHAT_COMPLETION_MODEL,
-        "messages":[
+    return await chatCompletion(
+        [
             {
-                "role":"user",
-                "content":input
+                "role": "user",
+                "content": input
             }
         ]
-    });
+    );
 }
